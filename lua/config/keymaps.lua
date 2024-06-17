fd424476a5ef32b55b2a9e8f985ab341f2e41b5a
bje259 <bradley.euell@gmail.com>
2024-06-17 13:59:02 -0500

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,13 +8,18 @@
 -- map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 -- map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
 
 -- Map <C-n> to toggle NERDTree
-vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
 
 -- Map <leader>p to open Files
-vim.api.nvim_set_keymap('n', '<leader>p', ':Files<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>p', ':Files<CR>', { noremap = true, silent = true })
 
 -- Map <Leader><Leader> to clear search highlighting
 vim.api.nvim_set_keymap('n', '<Leader><Leader>', ':nohlsearch<CR>', { noremap = true, silent = true })
